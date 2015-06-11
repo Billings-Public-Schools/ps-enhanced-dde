c070a0b6d9fd2b35c7a77315d5a6b94d5567f64d
Steven Riehl <sriehl@gmail.com>
2015-06-11 15:56:56 -0600

    move to the require syntax

--- web_root/scripts/c_dde-ext.js
+++ web_root/scripts/c_dde-ext.js
@@ -1,17 +1,22 @@
-var $j = jQuery.noConflict();
-$j(function(){
-	var extras = 3;
-	var template = $j('#search-template');
-	if(template !=undefined){
-		for(var i = extras; i >0; i--){
-			var clone = $j(template).clone().removeAttr('id');
-			$j('select[name="fieldnum_2"]', clone).attr('name', 'fieldnum_'+(i+2))
-				.removeAttr('onchange')
-				.change(function(){
-					set_comp(this, $j(this).parent().next('span').find('select').get(0));
-				});
-			$j('select[name="comparator2"]', clone).attr('name', 'comparator'+(i+2));
-			$j(template).after(clone);
-		}
-	}
+require(['jquery'], function($) {
+    $(function() {
+        'use strict';
+
+        var extras = 3,
+            template = $('select[name="fieldnum_2"]').parent().parent();
+
+        if (template != undefined) {
+            for (var i = extras; i >0; i--) {
+                var clone = $(template).clone().removeAttr('id');
+                $('select[name="fieldnum_2"]', clone).attr('name', 'fieldnum_'+(i+2))
+                    .removeAttr('onchange')
+                    .change(function(){
+                        set_comp(this, $(this).parent().next('span').find('select').get(0));
+                    });
+                $('select[name="comparator2"]', clone).attr('name', 'comparator'+(i+2));
+                $(template).after(clone);
+            }
+        }
+
+    });
 });
