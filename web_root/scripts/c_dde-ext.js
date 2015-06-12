8fe9a648ec3e1b0cda1227f597270135a9763f27
Steven Riehl <sriehl@gmail.com>
2015-06-12 11:26:07 -0600

    Allow adding and removing criteria
    This allows you to add as many criteria as you want as well as remove those you don't.

--- web_root/scripts/c_dde-ext.js
+++ web_root/scripts/c_dde-ext.js
@@ -2,31 +2,46 @@ require(['jquery'], function($) {
     $(function() {
         'use strict';
         
-        var addCriteria = function() {
-            var template,
-                newCriteria,
-                fieldSelect,
-                re,
-                newIdNumber;
+        var renumberCriteria = function() {
+            var criteria = $('select[name^="fieldnum_"]').parent().parent();
             
-            template = $('select[name^="fieldnum_"]').last().parent().parent();
-            newCriteria = template.clone();
-            fieldSelect = newCriteria.children().first().children();
+            criteria.each(function(i, c) {
+                var children = $(c).children().children(),
+                    field1,
+                    field2;
+                field1 = $(children[0]);
+                field2 = $(children[1]);
+                
+                field1.attr('name', 'fieldnum_' + (i+1));
+                field1.attr('onchange', 'set_comp(this, this.form.comparator' + (i+1) + ')');
+                field2.attr('name', 'comparator' + (i+1));
+            });
             
-            re = /fieldnum_(\d+)/;
-            newIdNumber = parseInt(re.exec(fieldSelect.attr('name'))[1]) + 1;
-            fieldSelect.attr('name', 'fieldnum_' + newIdNumber);
-            fieldSelect.attr('onchange', 'set_comp(this, this.form.comparator' + newIdNumber + ')');
-            
-            fieldSelect.parent().siblings().first().children().attr('name', 'comparator' + newIdNumber);
-            
-            template.after(newCriteria);
-            
-            return false;
         };
         
-        $('input[name="applyschoolfilter"]').before('<div class="button-row"><button id="addCriteria">Add Criteria</button></div>');
+        var addCriteria = function(e) {
+            e.preventDefault();
+            
+            var lastCriteria = $('select[name^="fieldnum_"]').parent().parent().last();
+            lastCriteria.after(lastCriteria.clone());
+            
+            renumberCriteria();
+        };
+        
+        var removeCriteria = function(e) {
+            e.preventDefault();
+            
+            $(this).parent().parent().remove();
+            
+            renumberCriteria();
+        };
+        
+        $('input[name="applyschoolfilter"]').parent().append('<button id="addCriteria" style="margin: 5px; float:right">Add Criteria</button>');
         $('#addCriteria').on('click', addCriteria);
+        $('#addCriteria').parent().attr('style', 'background-color: white');
+        
+        $('input[name="value"]').after('<button class="removeButton" style="margin: 0 5px">X</button>');
+        $('fieldset.inline').on('click', '.removeButton', removeCriteria);
         
     });
 });
