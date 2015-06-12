3073e9bd08cda49dcf9c79f8d8c960da2223e101
Steven Riehl <sriehl@gmail.com>
2015-06-12 13:08:25 -0600

    Add a few comments

--- web_root/scripts/c_dde-ext.js
+++ web_root/scripts/c_dde-ext.js
@@ -3,6 +3,7 @@ require(['jquery'], function($) {
         'use strict';
         
         var renumberCriteria = function() {
+            // get all the criteria divs
             var criteria = $('select[name^="fieldnum_"]').parent().parent();
             
             criteria.each(function(i, c) {
@@ -12,6 +13,7 @@ require(['jquery'], function($) {
                 field1 = $(children[0]);
                 field2 = $(children[1]);
                 
+                // update the field names
                 field1.attr('name', 'fieldnum_' + (i+1));
                 field1.attr('onchange', 'set_comp(this, this.form.comparator' + (i+1) + ')');
                 field2.attr('name', 'comparator' + (i+1));
@@ -20,26 +22,40 @@ require(['jquery'], function($) {
         };
         
         var addCriteria = function(e) {
+            // prevent submitting the form
             e.preventDefault();
             
-            var lastCriteria = $('select[name^="fieldnum_"]').parent().parent().last();
-            lastCriteria.after(lastCriteria.clone());
+            var lastCriteria,
+                newCriteria;
+            
+            lastCriteria = $('select[name^="fieldnum_"]').parent().parent().last();
+            newCriteria = lastCriteria.clone();
+            
+            // clear out any values left over
+            newCriteria.children().children().val('');
+            
+            // add the new critera to the page
+            lastCriteria.after(newCriteria);
             
             renumberCriteria();
         };
         
         var removeCriteria = function(e) {
+            // prevent submitting the form
             e.preventDefault();
             
+            // remove the div of the grandparent of the button clicked
             $(this).parent().parent().remove();
             
             renumberCriteria();
         };
         
+        // create the button the add critera
         $('input[name="applyschoolfilter"]').parent().append('<button id="addCriteria" style="margin: 5px; float:right">Add Criteria</button>');
         $('#addCriteria').on('click', addCriteria);
         $('#addCriteria').parent().attr('style', 'background-color: white');
         
+        // create the removeCritera button
         $('input[name="value"]').after('<button class="removeButton" style="margin: 0 5px">X</button>');
         $('fieldset.inline').on('click', '.removeButton', removeCriteria);
         
